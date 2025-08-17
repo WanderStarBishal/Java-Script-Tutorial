d7df8e0a13a27f05dd3bea6495cdc4dad58f848b
Bishal Mandal <bishalmandalg157@gmail.com>
2025-08-17 08:01:18 +0000

    Learnt conversion operations

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = true;
+
+// console.log(typeof(score));
+// console.log(typeof score);
+// console.log(score);
+
+let ValueInNumber = Number(score);
+// console.log(typeof(ValueInNumber));
+// console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 or false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = "bishal";
+let booleanLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+
+// 0 => true or 1 => false
+// "" => false
+
+let someNumber = 30;
+
