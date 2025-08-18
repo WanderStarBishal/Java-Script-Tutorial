026ecbacce607d65c3808caaec5488cfc45ee33c
Bishal Mandal <bishalmandalg157@gmail.com>
2025-08-18 16:31:00 +0000

    learnt some things !

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,22 @@
+const gamename = new String('Keshav apna Chutad');
+
+// console.log(gamename.__proto__);
+
+// console.log(gamename.toUpperCase());
+// console.log(gamename.length);
+// console.log(gamename.toLowerCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('C'));
+// console.log(gamename.substring(0,6));
+// console.log(gamename.slice(-6,12));
+
+// const newname = '    hitesh    ';
+// console.log(newname);
+// console.log(newname.trim());
+
+const url = "https://bishal.com/make%20believe";
+console.log(url.replace('%20','_'));
+
+console.log(url.includes("Keshav Chudad"));
+
+console.log(`$(gamename) dene aa gaya hai bahana mar ke video here : $(url) `);
